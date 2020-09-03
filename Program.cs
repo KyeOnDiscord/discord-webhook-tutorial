3de125ac06a23bbcfb8fc29f4c26c15cd4de20db
Kye <36981621+ProMasterBoy@users.noreply.github.com>
2020-09-03 20:12:04 +1000

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using System.Net;
+using System.Collections.Specialized;
+public class Program
+{
+	public static void sendDiscordWebhook(string URL, string profilepic, string username, string message)
+        {
+            using (WebClient web = new WebClient())
+            {
+                NameValueCollection discordValues = new NameValueCollection();
+                discordValues.Clear();
+                discordValues.Add("username", username);
+                discordValues.Add("avatar_url", profilepic);
+                discordValues.Add("content", message);
+                web.UploadValues(URL, discordValues);
+            }
+        }
+	
+	public static void Main()
+	{
+		sendDiscordWebhook("https://discordapp.com/api/webhooks/749445508849860740/b_6Xv2hnhyqnWahXiAPefSv_s5cNXPZ8mg4sw4dnvrX5PUVaqjHY9Rqglfc9v5WFOLfM", "https://i.imgur.com/b2gkIWC.png", "John Doe", "Test Message");
+	}
+}
